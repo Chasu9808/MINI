ab56a344052ec4e97c8c6d36360aef3a556b185a
Limchasu <mayblue20@naver.com>
2024-04-12 18:53:51 +0900

    project edit

---
+++ JS/main.js
@@ -0,0 +1,14 @@
+// main.js
+
+function redirectToMain() {
+    window.location.href = "main.html";
+}
+
+
+// JavaScript를 사용하여 로그인 버튼 클릭 시 main.html로 이동
+document.getElementById("login-form").addEventListener("submit", function(event) {
+    event.preventDefault(); // 기본 이벤트(폼 제출) 방지
+    window.location.href = "main.html"; // 페이지 이동
+});
+
+
