77f89ce52a5fcf83e2806bada92153f7e05e68b4
Limchasu <mayblue20@naver.com>
2024-04-14 19:34:04 +0900

    commit

--- JS/resetPassword.js
+++ JS/resetPassword.js
@@ -3,9 +3,20 @@ document.getElementById("resetForm").addEventListener("submit", function(event) 
 
     var email = document.getElementById("email").value;
     
-    // 비밀번호 재설정 이메일 전송
-    sendResetEmail(email);
+    // 이메일 주소 확인
+    if (validateEmail(email)) {
+        // 비밀번호 재설정 이메일 전송
+        sendResetEmail(email);
+    } else {
+        alert("유효한 이메일 주소를 입력하세요.");
+    }
 });
+
+function validateEmail(email) {
+    // 간단한 이메일 유효성 검사 (실제로는 보다 엄격한 검사가 필요)
+    var re = /\S+@\S+\.\S+/;
+    return re.test(email);
+}
 
 function sendResetEmail(email) {
     // 이 함수는 이메일을 전송하는 서버 측 코드로 대체되어야 합니다.
