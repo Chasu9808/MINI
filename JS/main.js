a398f1aae46ec8d1f83edb01050a778c1c908dcc
Limchasu <mayblue20@naver.com>
2024-04-14 22:47:51 +0900

    웹개발

--- JS/main.js
+++ JS/main.js
@@ -11,18 +11,5 @@ document.getElementById("login-form").addEventListener("submit", function(event)
     window.location.href = "main.html"; // 페이지 이동
 });
 
-window.onload = function() {
-    // 하위 항목 박스의 크기를 동적으로 조정
-    adjustDropdownSize();
-};
-
-function adjustDropdownSize() {
-    var dropdown = document.getElementById("gameListDropdown");
-    var itemsCount = dropdown.getElementsByTagName("a").length;
-    var itemWidth = 10; // 각 항목의 기본 너비
-
-    // 하위 항목 박스의 너비를 항목 수에 따라 조정
-    dropdown.style.width = (itemsCount * itemWidth) + "px";
-}
 
 
