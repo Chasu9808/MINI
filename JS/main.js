6aa2ac199a8a452b03b02015fd7f10479eed13c3
Limchasu <mayblue20@naver.com>
2024-04-16 10:49:00 +0900

    login 완료창 생성

--- JS/main.js
+++ JS/main.js
@@ -8,7 +8,7 @@ function redirectToMain() {
 // JavaScript를 사용하여 로그인 버튼 클릭 시 main.html로 이동
 document.getElementById("login-form").addEventListener("submit", function(event) {
     event.preventDefault(); // 기본 이벤트(폼 제출) 방지
-    window.location.href = "index.html"; // 페이지 이동
+    window.location.href = "mainlogin.html"; // 페이지 이동
 });
 
 function showText(textId) {
--- JS/mainlogin.js
+++ JS/mainlogin.js
@@ -1,16 +1,3 @@
-// main.js
-
-function redirectToMain() {
-    window.location.href = "index.html";
-}
-
-
-// JavaScript를 사용하여 로그인 버튼 클릭 시 main.html로 이동
-document.getElementById("login-form").addEventListener("submit", function(event) {
-    event.preventDefault(); // 기본 이벤트(폼 제출) 방지
-    window.location.href = "indexlogin.html"; // 페이지 이동
-});
-
 function showText(textId) {
     var text = document.getElementById(textId);
     text.style.visibility = 'visible'; // 마우스 커서가 이미지 위에 있을 때 텍스트를 보이도록 설정
